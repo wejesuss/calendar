e8bea75aacd418075dd6f09b275583e72cb60a22
Wemerson Jesus <wejesuss@gmail.com>
2022-03-06 20:40:38 -0300

    test: ensure CreateSessionController can normalize time

--- src/presentation/controllers/create-session/create-session.spec.ts
+++ src/presentation/controllers/create-session/create-session.spec.ts
@@ -506,4 +506,17 @@ describe('Create Session Controller', () => {
     httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('session_date')))
   })
+
+  test('Should have normalizeTime and normalize just hours', async () => {
+    const { sut } = makeSut()
+
+    const hours = sut.normalizeTime('09', 0)
+    const normalizedHours = sut.normalizeTime('00', 0)
+    const minutes = sut.normalizeTime('15', 1)
+
+    expect(sut).toHaveProperty('normalizeTime')
+    expect(hours).toBe(9)
+    expect(normalizedHours).toBe(24)
+    expect(minutes).toBe(15)
+  })
 })
