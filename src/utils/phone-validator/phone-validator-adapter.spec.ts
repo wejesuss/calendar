0b8288ef588bfd4e3222dcb63ed1052b9abe4391
Wemerson Jesus <wejesuss@gmail.com>
2022-03-23 14:03:03 -0300

    test: ensure PhoneValidatorAdapter returns true if validator returns true

--- src/utils/phone-validator/phone-validator-adapter.spec.ts
+++ src/utils/phone-validator/phone-validator-adapter.spec.ts
@@ -1,10 +1,25 @@
 import { PhoneValidatorAdapter } from './phone-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isMobilePhone (): boolean {
+    return true
+  }
+}))
 
 describe('PhoneValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new PhoneValidatorAdapter()
+    jest.spyOn(validator, 'isMobilePhone').mockReturnValueOnce(false)
 
     const isValid = sut.isValid('99911111111')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new PhoneValidatorAdapter()
+
+    const isValid = sut.isValid('99911111111')
+    expect(isValid).toBe(true)
   })
 })
