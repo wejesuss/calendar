d7b0fc0ed3fcb43960c4f7779154c242bfe25444
Wemerson Jesus <wejesuss@gmail.com>
2022-03-30 17:50:32 -0300

    test: ensure CPFValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator/cpf-validator-adapter.spec.ts
+++ src/utils/cpf-validator/cpf-validator-adapter.spec.ts
@@ -1,10 +1,25 @@
 import { CPFValidatorAdapter } from './cpf-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isTaxID (cpf: string): boolean {
+    return true
+  }
+}))
 
 describe('CPFValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new CPFValidatorAdapter()
+    jest.spyOn(validator, 'isTaxID').mockReturnValueOnce(false)
 
     const isValid = sut.isValid('11111111111')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new CPFValidatorAdapter()
+
+    const isValid = sut.isValid('11111111111')
+    expect(isValid).toBe(true)
   })
 })
