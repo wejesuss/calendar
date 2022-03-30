25b30940f6529fe11d5fdd353ff0c789ddd4a072
Wemerson Jesus <wejesuss@gmail.com>
2022-03-30 20:18:01 -0300

    test: ensure SessionDateValidatorAdapter returns false if validator returns false

--- src/utils/session-date-validator/session-date-validator-adapter.spec.ts
+++ src/utils/session-date-validator/session-date-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { SessionDateValidatorAdapter } from './session-date-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isDate (str: string, options: validator.IsDateOptions): boolean {
+    return true
+  }
+}))
 
 describe('SessionDateValidator Adapter', () => {
   test('Should return false if session date is not a string', () => {
@@ -13,6 +20,15 @@ describe('SessionDateValidator Adapter', () => {
     const sut = new SessionDateValidatorAdapter()
 
     const isSessionDateValid = sut.isValid('')
+
+    expect(isSessionDateValid).toBe(false)
+  })
+
+  test('Should return false if validator returns false', () => {
+    const sut = new SessionDateValidatorAdapter()
+    jest.spyOn(validator, 'isDate').mockReturnValueOnce(false)
+
+    const isSessionDateValid = sut.isValid('2022.03.30')
 
     expect(isSessionDateValid).toBe(false)
   })
