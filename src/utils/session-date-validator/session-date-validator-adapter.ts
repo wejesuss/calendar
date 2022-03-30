b5e42c5af3e5558dc85ea6a242d9f386dd1ca2a5
Wemerson Jesus <wejesuss@gmail.com>
2022-03-30 20:17:47 -0300

    feat: ensure SessionDateValidatorAdapter returns false if validator returns false

--- src/utils/session-date-validator/session-date-validator-adapter.ts
+++ src/utils/session-date-validator/session-date-validator-adapter.ts
@@ -1,7 +1,10 @@
 import { SessionDateValidator } from '../../presentation/protocols/session-date-validator'
+import validator from 'validator'
 
 export class SessionDateValidatorAdapter implements SessionDateValidator {
   isValid (sessionDate: string): boolean {
     if (!sessionDate || typeof sessionDate !== 'string') return false
+
+    return validator.isDate(sessionDate, { delimiters: ['/'] })
   }
 }
