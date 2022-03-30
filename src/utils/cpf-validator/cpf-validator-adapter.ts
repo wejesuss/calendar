86c1439faaf82390459b06ed40b7c40506080873
Wemerson Jesus <wejesuss@gmail.com>
2022-03-30 17:50:10 -0300

    feat: ensure CPFValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator/cpf-validator-adapter.ts
+++ src/utils/cpf-validator/cpf-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { CPFValidator } from '../../presentation/protocols/cpf-validator'
+import validator from 'validator'
 
 export class CPFValidatorAdapter implements CPFValidator {
   isValid (cpf: string): boolean {
-    return false
+    return validator.isTaxID(cpf, 'pt-BR')
   }
 }
