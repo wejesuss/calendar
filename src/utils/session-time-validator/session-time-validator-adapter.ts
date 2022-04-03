f3584bd61a87b10dc590f42c1f543f7e47f34e23
Wemerson Jesus <wejesuss@gmail.com>
2022-04-03 16:14:05 -0300

    feat: ensure SessionTimeValidatorAdapter returns false if session time is incorrectly formatted

--- src/utils/session-time-validator/session-time-validator-adapter.ts
+++ src/utils/session-time-validator/session-time-validator-adapter.ts
@@ -3,5 +3,8 @@ import { SessionTimeValidator } from '../../presentation/protocols/session-time-
 export class SessionTimeValidatorAdapter implements SessionTimeValidator {
   isValid (sessionTime: string): boolean {
     if (typeof sessionTime !== 'string' || !sessionTime) return false
+
+    const timeFormat = /^\d{2}:\d{2}(:\d{2})?$/
+    return timeFormat.test(sessionTime)
   }
 }
