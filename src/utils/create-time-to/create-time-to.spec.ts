5ae4d693f03dec1f3bb9e70065f4b8896fa8faae
Wemerson Jesus <wejesuss@gmail.com>
2022-04-06 17:32:29 -0300

    test: ensure CreateTimeToAdapter returns empty string if time to is less than time from

--- src/utils/create-time-to/create-time-to.spec.ts
+++ src/utils/create-time-to/create-time-to.spec.ts
@@ -16,4 +16,14 @@ describe('CreateTimeTo Adapter', () => {
     timeTo = sut.create('23:59', 1)
     expect(timeTo).toBe('00:00')
   })
+
+  test('Should return empty string if time to is less than time from', () => {
+    const sut = new CreateTimeToAdapter()
+
+    let timeTo = sut.create('22:00', 145)
+    expect(timeTo).toBe('') // 00:25
+
+    timeTo = sut.create('23:00', 145)
+    expect(timeTo).toBe('') // 01:25
+  })
 })
