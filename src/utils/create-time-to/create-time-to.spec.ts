7909ade0cfbc7c367d6b48547e41d2b372323556
Wemerson Jesus <wejesuss@gmail.com>
2022-04-06 17:30:01 -0300

    test: ensure CreateTimeToAdapter creates and returns time to using duration and time from

---
+++ src/utils/create-time-to/create-time-to.spec.ts
@@ -0,0 +1,19 @@
+import { CreateTimeToAdapter } from './create-time-to'
+
+describe('CreateTimeTo Adapter', () => {
+  test('Should return time to using duration and time from', () => {
+    const sut = new CreateTimeToAdapter()
+
+    let timeTo = sut.create('09:30', 15)
+    expect(timeTo).toBe('09:45')
+
+    timeTo = sut.create('09:50', 15)
+    expect(timeTo).toBe('10:05')
+
+    timeTo = sut.create('23:00', 60)
+    expect(timeTo).toBe('00:00')
+
+    timeTo = sut.create('23:59', 1)
+    expect(timeTo).toBe('00:00')
+  })
+})
