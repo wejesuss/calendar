c0ccc9e4731189a274dafca5c5cceff0406e69d5
Wemerson Jesus <wejesuss@gmail.com>
2022-04-08 16:48:02 -0300

    test: ensure DbAddSession calls AddSessionRepository with correct values

---
+++ src/data/add-session/db-add-session.spec.ts
@@ -0,0 +1,55 @@
+import { Session } from '../../domain/models/session'
+import { AddSessionModel } from '../../domain/usecases/add-session'
+import { AddSessionRepository } from '../protocols/add-session-repository'
+import { DbAddSession } from './db-add-session'
+
+interface SutTypes {
+  sut: DbAddSession
+  addSessionRepositoryStub: AddSessionRepository
+}
+
+const makeSut = (): SutTypes => {
+  class AddSessionRepository {
+    async add (sessionData: AddSessionModel): Promise<Session> {
+      return null
+    }
+  }
+
+  const addSessionRepositoryStub = new AddSessionRepository()
+  const sut = new DbAddSession(addSessionRepositoryStub)
+
+  return { sut, addSessionRepositoryStub }
+}
+
+describe('DbAddSession', () => {
+  test('Should call AddSessionRepository with correct values', async () => {
+    const { addSessionRepositoryStub, sut } = makeSut()
+    const addSessionSpy = jest.spyOn(addSessionRepositoryStub, 'add')
+
+    const sessionData = {
+      s_date: '2022/01/22',
+      time_from: '10:30',
+      time_to: '11:30',
+      duration: 60,
+      name: 'foo',
+      email: 'foo@example.com',
+      cpf: '11111111111',
+      phone: '111111111',
+      description: 'any description'
+    }
+
+    await sut.add(sessionData)
+
+    expect(addSessionSpy).toHaveBeenCalledWith({
+      s_date: '2022-01-22',
+      time_from: '10:30',
+      time_to: '11:30',
+      duration: 60,
+      name: 'foo',
+      email: 'foo@example.com',
+      cpf: '11111111111',
+      phone: '111111111',
+      description: 'any description'
+    })
+  })
+})
