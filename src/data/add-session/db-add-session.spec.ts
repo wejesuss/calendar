ad29f55625cf7613ff52330326525fcc5a04baee
Wemerson Jesus <wejesuss@gmail.com>
2022-04-08 16:55:41 -0300

    refactor: create session data with a helper function

--- src/data/add-session/db-add-session.spec.ts
+++ src/data/add-session/db-add-session.spec.ts
@@ -3,6 +3,18 @@ import { AddSessionModel } from '../../domain/usecases/add-session'
 import { AddSessionRepository } from '../protocols/add-session-repository'
 import { DbAddSession } from './db-add-session'
 
+const makeFakeAddSessionModel = (): AddSessionModel => ({
+  s_date: '2022/01/22',
+  time_from: '10:30',
+  time_to: '11:30',
+  duration: 60,
+  name: 'foo',
+  email: 'foo@example.com',
+  cpf: '11111111111',
+  phone: '111111111',
+  description: 'any description'
+})
+
 const makeAddSessionRepository = (): AddSessionRepository => {
   class AddSessionRepository {
     async add (sessionData: AddSessionModel): Promise<Session> {
@@ -30,30 +42,10 @@ describe('DbAddSession', () => {
     const { addSessionRepositoryStub, sut } = makeSut()
     const addSessionSpy = jest.spyOn(addSessionRepositoryStub, 'add')
 
-    const sessionData = {
-      s_date: '2022/01/22',
-      time_from: '10:30',
-      time_to: '11:30',
-      duration: 60,
-      name: 'foo',
-      email: 'foo@example.com',
-      cpf: '11111111111',
-      phone: '111111111',
-      description: 'any description'
-    }
+    const sessionModel = makeFakeAddSessionModel()
+    const addSessionData = Object.assign({}, sessionModel, { s_date: '2022-01-22' })
 
-    await sut.add(sessionData)
-
-    expect(addSessionSpy).toHaveBeenCalledWith({
-      s_date: '2022-01-22',
-      time_from: '10:30',
-      time_to: '11:30',
-      duration: 60,
-      name: 'foo',
-      email: 'foo@example.com',
-      cpf: '11111111111',
-      phone: '111111111',
-      description: 'any description'
-    })
+    await sut.add(sessionModel)
+    expect(addSessionSpy).toHaveBeenCalledWith(addSessionData)
   })
 })
