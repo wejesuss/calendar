dbf6d51f0db3e05f66a72eb7c0ad4349f56b90d6
Wemerson Jesus <wejesuss@gmail.com>
2022-04-08 17:14:57 -0300

    feat: ensure DbAddSession calls AddSessionRepository with correct price value

--- src/data/add-session/db-add-session.ts
+++ src/data/add-session/db-add-session.ts
@@ -1,14 +1,17 @@
 import { AddSession, AddSessionModel, AddSessionRepository, Session } from './db-add-session-protocols'
 
 export class DbAddSession implements AddSession {
-  constructor (private readonly addSessionRepository: AddSessionRepository) { }
+  constructor (
+    private readonly price: number,
+    private readonly addSessionRepository: AddSessionRepository
+  ) { }
 
   async add (sessionData: AddSessionModel): Promise<Session> {
     const sessionDate = sessionData.s_date.split('/').join('-')
 
     await this.addSessionRepository.add(Object.assign(
       {}, sessionData,
-      { s_date: sessionDate }
+      { s_date: sessionDate, price: this.price }
     ))
 
     return null
