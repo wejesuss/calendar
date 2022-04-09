66591973cb6fab7bb613ab9a193ce9268129398c
Wemerson Jesus <wejesuss@gmail.com>
2022-04-09 10:34:00 -0300

    test: ensure DbGetSchedule returns schedule on success

--- src/data/get-schedule/db-get-schedule.spec.ts
+++ src/data/get-schedule/db-get-schedule.spec.ts
@@ -8,7 +8,24 @@ const makeFakeScheduleOptions = (): GetScheduleOptions => ({
   year: 2022
 })
 
-const makeFakeSchedule = (): PartialSchedule => ({
+const makeFakeSchedule = (): Schedule => ({
+  id: 1,
+  duration: 15,
+  activation_interval: 2,
+  activation_interval_type: 30,
+  availability_0: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_1: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_2: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_3: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_4: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_5: [{ time_from: '09:00', time_to: '12:30' }],
+  availability_6: [{ time_from: '09:00', time_to: '12:30' }],
+  replacements: [],
+  created_at: 1649511107538,
+  updated_at: 1649511107538
+})
+
+const makeFakePartialSchedule = (): PartialSchedule => ({
   duration: 15,
   activation_interval: 2,
   activation_interval_type: 30,
@@ -19,11 +36,11 @@ const makeFakeSchedule = (): PartialSchedule => ({
 const makeGetScheduleRepository = (): GetScheduleRepository => {
   class GetScheduleRepository {
     async getAll (): Promise<Schedule> {
-      return null
+      return makeFakeSchedule()
     }
 
     async getPartial (scheduleOptions?: GetScheduleOptions): Promise<PartialSchedule> {
-      return makeFakeSchedule()
+      return makeFakePartialSchedule()
     }
   }
 
@@ -59,6 +76,13 @@ describe('DbGetSchedule', () => {
       const promise = sut.getAll()
       await expect(promise).rejects.toThrow()
     })
+
+    test('Should return schedule on success', async () => {
+      const { sut } = makeSut()
+
+      const schedule = await sut.getAll()
+      expect(schedule).toEqual(makeFakeSchedule())
+    })
   })
 
   describe('getPartial', () => {
@@ -88,7 +112,7 @@ describe('DbGetSchedule', () => {
       const scheduleOptions = makeFakeScheduleOptions()
       const schedule = await sut.getPartial(scheduleOptions)
 
-      expect(schedule).toEqual(makeFakeSchedule())
+      expect(schedule).toEqual(makeFakePartialSchedule())
     })
   })
 })
