a251fd5cbadaf511d69e5daed63e7702abde30cb
Wemerson Jesus <wejesuss@gmail.com>
2022-04-21 15:53:02 -0300

    refactor: use helper functions to create fake objects

--- src/infra/repositories/prisma/prisma-session-repository/prisma-session-repository.spec.ts
+++ src/infra/repositories/prisma/prisma-session-repository/prisma-session-repository.spec.ts
@@ -1,6 +1,32 @@
 import { AddSessionModel, PrismaClient } from './prisma-session-repository-protocols'
 import { PrismaSessionRepository } from './prisma-session-repository'
 
+const makeFakeSessionData = (): AddSessionModel => ({
+  s_date: '2022-01-22',
+  time_from: '09:00',
+  time_to: '10:00',
+  duration: 60,
+  name: 'any_name',
+  email: 'any_email@example.com',
+  cpf: '11111111111',
+  phone: '11111111111',
+  description: '',
+  price: 10000
+})
+
+const makeFakeAddSessionData = (sessionData: AddSessionModel): any => ({
+  duration: 60,
+  name: 'any_name',
+  email: 'any_email@example.com',
+  cpf: '11111111111',
+  phone: '11111111111',
+  description: '',
+  price: 10000,
+  sDate: new Date(`2022-01-22 ${sessionData.time_from}`),
+  timeFrom: new Date(`2022-01-22 ${sessionData.time_from}`),
+  timeTo: new Date(`2022-01-22 ${sessionData.time_to}`)
+})
+
 interface SutTypes {
   sut: PrismaSessionRepository
   prisma: PrismaClient
@@ -18,34 +44,10 @@ describe('PrismaSessionRepository', () => {
     const { sut, prisma } = makeSut()
     const createSessionSpy = jest.spyOn(prisma.session, 'create')
 
-    const sessionData: AddSessionModel = {
-      s_date: '2022-01-22',
-      time_from: '09:00',
-      time_to: '10:00',
-      duration: 60,
-      name: 'any_name',
-      email: 'any_email@example.com',
-      cpf: '11111111111',
-      phone: '11111111111',
-      description: '',
-      price: 10000
-    }
-
+    const sessionData = makeFakeSessionData()
     await sut.add(sessionData)
 
-    const addSessionData = {
-      duration: 60,
-      name: 'any_name',
-      email: 'any_email@example.com',
-      cpf: '11111111111',
-      phone: '11111111111',
-      description: '',
-      price: 10000,
-      sDate: new Date(`2022-01-22 ${sessionData.time_from}`),
-      timeFrom: new Date(`2022-01-22 ${sessionData.time_from}`),
-      timeTo: new Date(`2022-01-22 ${sessionData.time_to}`)
-    }
-
+    const addSessionData = makeFakeAddSessionData(sessionData)
     expect(createSessionSpy).toHaveBeenCalledWith({ data: addSessionData })
   })
 })
