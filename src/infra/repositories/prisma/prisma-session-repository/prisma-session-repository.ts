27614b34bbc9d4a221c881edadb57af59deb8e3e
Wemerson Jesus <wejesuss@gmail.com>
2022-04-22 15:30:23 -0300

    feat: ensure PrismaSessionRepository receives encryption key on constructor

--- src/infra/repositories/prisma/prisma-session-repository/prisma-session-repository.ts
+++ src/infra/repositories/prisma/prisma-session-repository/prisma-session-repository.ts
@@ -1,53 +1,57 @@
 import { AddSessionRepository, Session, AddSessionModel, PrismaClient, Prisma } from './prisma-session-repository-protocols'
 
 export class PrismaSessionRepository implements AddSessionRepository {
-  constructor (private readonly prisma: PrismaClient) {}
+   readonly #encryptionKey: string
 
-  createInsertionQuery (sessionData: AddSessionModel): Prisma.Sql {
-    const {
-      name, cpf, description, duration, email, phone, price,
-      s_date: sDate,
-      time_from: timeFrom,
-      time_to: timeTo,
-      user_id: userId
-    } = sessionData
+   constructor (private readonly prisma: PrismaClient, encryptionKey: string) {
+     this.#encryptionKey = encryptionKey
+   }
 
-    const dateFrom = new Date(`${sDate} ${timeFrom} GMT-00`)
-    const dateTo = new Date(`${sDate} ${timeTo} GMT-00`)
+   createInsertionQuery (sessionData: AddSessionModel): Prisma.Sql {
+     const {
+       name, cpf, description, duration, email, phone, price,
+       s_date: sDate,
+       time_from: timeFrom,
+       time_to: timeTo,
+       user_id: userId
+     } = sessionData
 
-    const cpfEncrypted = Prisma.sql`pgp_sym_encrypt(${cpf}, ${'longsecretencryptionkey'})`
-    const phoneEncrypted = Prisma.sql`pgp_sym_encrypt(${phone}, ${'longsecretencryptionkey'})`
-    const id = Prisma.sql`gen_random_uuid()`
+     const dateFrom = new Date(`${sDate} ${timeFrom} GMT-00`)
+     const dateTo = new Date(`${sDate} ${timeTo} GMT-00`)
 
-    return Prisma.sql`INSERT INTO session (id,name,email,cpf,phone,description,duration,s_date,time_from,time_to,price,user_id) VALUES (${id},${name},${email},${cpfEncrypted},${phoneEncrypted},${description},${duration},${dateFrom},${dateFrom},${dateTo},${price},${userId}) RETURNING *;`
-  }
+     const cpfEncrypted = Prisma.sql`pgp_sym_encrypt(${cpf}, ${this.#encryptionKey})`
+     const phoneEncrypted = Prisma.sql`pgp_sym_encrypt(${phone}, ${this.#encryptionKey})`
+     const id = Prisma.sql`gen_random_uuid()`
 
-  async add (sessionData: AddSessionModel): Promise<Session> {
-    const {
-      name, cpf, description, duration, email, phone, price,
-      user_id: userId
-    } = sessionData
+     return Prisma.sql`INSERT INTO session (id,name,email,cpf,phone,description,duration,s_date,time_from,time_to,price,user_id) VALUES (${id},${name},${email},${cpfEncrypted},${phoneEncrypted},${description},${duration},${dateFrom},${dateFrom},${dateTo},${price},${userId}) RETURNING *;`
+   }
 
-    const sql = this.createInsertionQuery(sessionData)
-    const [session] = await this.prisma.$queryRaw<Session[]>(sql)
+   async add (sessionData: AddSessionModel): Promise<Session> {
+     const {
+       name, cpf, description, duration, email, phone, price,
+       user_id: userId
+     } = sessionData
 
-    return {
-      id: session.id,
-      name,
-      email,
-      cpf,
-      phone,
-      description,
-      duration,
-      s_date: session.s_date.split('-').join('/'),
-      time_from: session.time_from,
-      time_to: session.time_to,
-      price,
-      paid: session.paid,
-      user_id: userId,
-      image_path: session.image_path,
-      created_at: session.created_at,
-      updated_at: session.updated_at
-    }
-  }
+     const sql = this.createInsertionQuery(sessionData)
+     const [session] = await this.prisma.$queryRaw<Session[]>(sql)
+
+     return {
+       id: session.id,
+       name,
+       email,
+       cpf,
+       phone,
+       description,
+       duration,
+       s_date: session.s_date.split('-').join('/'),
+       time_from: session.time_from,
+       time_to: session.time_to,
+       price,
+       paid: session.paid,
+       user_id: userId,
+       image_path: session.image_path,
+       created_at: session.created_at,
+       updated_at: session.updated_at
+     }
+   }
 }
