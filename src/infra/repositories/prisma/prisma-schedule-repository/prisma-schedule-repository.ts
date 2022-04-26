d2a62b5b88dd018ecddcaaa4b9a40f9a60a84d90
Wemerson Jesus <wejesuss@gmail.com>
2022-04-26 18:00:41 -0300

    feat: ensure PrismaScheduleRepository returns partial schedule on success

--- src/infra/repositories/prisma/prisma-schedule-repository/prisma-schedule-repository.ts
+++ src/infra/repositories/prisma/prisma-schedule-repository/prisma-schedule-repository.ts
@@ -1,7 +1,59 @@
-import { Schedule, GetScheduleOptions, GetScheduleRepository, PartialSchedule, PrismaClient } from './prisma-schedule-repository-protocols'
+import { Schedule, GetScheduleOptions, GetScheduleRepository, PartialSchedule, PrismaClient, zeroPadder } from './prisma-schedule-repository-protocols'
 
 export class PrismaScheduleRepository implements GetScheduleRepository {
   constructor (private readonly prisma: PrismaClient) {}
+
+  mapSchedule (schedule: {
+    duration: number
+    activationInterval: number
+    activationIntervalType: number
+  }): {
+      duration: number
+      activation_interval: number
+      activation_interval_type: number
+    } {
+    return {
+      duration: schedule.duration,
+      activation_interval: schedule.activationInterval,
+      activation_interval_type: schedule.activationIntervalType
+    }
+  }
+
+  mapTimeInterval (timeInterval: {
+    week: number
+    timeFrom: Date
+    timeTo: Date
+  }): {week: number, time_from: string, time_to: string} {
+    const { timeFrom, timeTo, week } = timeInterval
+
+    return {
+      week,
+      time_from: `${zeroPadder.pad(timeFrom.getUTCHours())}:${zeroPadder.pad(timeFrom.getUTCMinutes())}`,
+      time_to: `${zeroPadder.pad(timeTo.getUTCHours())}:${zeroPadder.pad(timeTo.getUTCMinutes())}`
+    }
+  }
+
+  mapReplacement (replacement: {
+    rDate: Date
+    rTimeFrom: Date
+    rTimeTo: Date
+  }): {
+      date: string
+      time_from: string
+      time_to: string
+    } {
+    const { rDate, rTimeFrom, rTimeTo } = replacement
+
+    const date = `${zeroPadder.pad(rDate.getUTCFullYear())}/${zeroPadder.pad(rDate.getUTCMonth() + 1)}/${zeroPadder.pad(rDate.getUTCDate())}`
+    const timeFrom = `${zeroPadder.pad(rTimeFrom.getUTCHours())}:${zeroPadder.pad(rTimeFrom.getUTCMinutes())}`
+    const timeTo = `${zeroPadder.pad(rTimeTo.getUTCHours())}:${zeroPadder.pad(rTimeTo.getUTCMinutes())}`
+
+    return {
+      date: date,
+      time_from: timeFrom,
+      time_to: timeTo
+    }
+  }
 
   async getAll (): Promise<Schedule> {
     throw new Error('Not Implemented')
@@ -11,22 +63,30 @@ export class PrismaScheduleRepository implements GetScheduleRepository {
     const { weekDay, date, month, year } = scheduleOptions
     const rDate = new Date(Date.UTC(year, month - 1, date))
 
-    await this.prisma.schedule.findFirst({
+    const schedule = await this.prisma.schedule.findFirst({
       select: { duration: true, activationInterval: true, activationIntervalType: true }
     })
 
-    await this.prisma.timeInterval.findMany({
+    const timeIntervals = await this.prisma.timeInterval.findMany({
       select: { week: true, timeFrom: true, timeTo: true },
       orderBy: { timeFrom: 'asc' },
       where: { week: { equals: weekDay } }
     })
 
-    await this.prisma.replacement.findMany({
+    const replacements = await this.prisma.replacement.findMany({
       select: { rDate: true, rTimeFrom: true, rTimeTo: true },
       orderBy: { rTimeFrom: 'asc' },
       where: { rDate: { equals: rDate } }
     })
 
-    return null
+    const mappedSchedule = this.mapSchedule(schedule)
+    const mappedTimeIntervals = timeIntervals.map((timeInterval) => this.mapTimeInterval(timeInterval))
+    const mappedReplacements = replacements.map((replacement) => this.mapReplacement(replacement))
+
+    return {
+      ...mappedSchedule,
+      availability: mappedTimeIntervals,
+      replacements: mappedReplacements
+    }
   }
 }
