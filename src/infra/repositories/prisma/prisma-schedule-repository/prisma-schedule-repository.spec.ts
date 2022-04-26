a21087ecf2d87fbf7586d65cb40a07e8bbc884ab
Wemerson Jesus <wejesuss@gmail.com>
2022-04-26 16:47:42 -0300

    refactor: create helper functions to make argument options

--- src/infra/repositories/prisma/prisma-schedule-repository/prisma-schedule-repository.spec.ts
+++ src/infra/repositories/prisma/prisma-schedule-repository/prisma-schedule-repository.spec.ts
@@ -1,5 +1,40 @@
-import { PrismaClient } from './prisma-schedule-repository-protocols'
+import { GetScheduleOptions, PrismaClient, Prisma, zeroPadder } from './prisma-schedule-repository-protocols'
 import { PrismaScheduleRepository } from './prisma-schedule-repository'
+
+const makeFakeScheduleOptions = (): GetScheduleOptions => ({
+  weekDay: 6,
+  year: 2022,
+  month: 1,
+  date: 22
+})
+
+const makeFindScheduleOptions = (): Prisma.ScheduleFindFirstArgs => ({
+  select: {
+    duration: true,
+    activationInterval: true,
+    activationIntervalType: true
+  }
+})
+
+const makeFindTimeIntervalOptions = (scheduleOptions: GetScheduleOptions): Prisma.TimeIntervalFindManyArgs => ({
+  select: {
+    week: true,
+    timeFrom: true,
+    timeTo: true
+  },
+  orderBy: { timeFrom: 'asc' },
+  where: { week: { equals: scheduleOptions.weekDay } }
+})
+
+const makeFindReplacementOptions = (scheduleOptions: GetScheduleOptions): Prisma.ReplacementFindManyArgs => ({
+  select: {
+    rDate: true,
+    rTimeFrom: true,
+    rTimeTo: true
+  },
+  orderBy: { rTimeFrom: 'asc' },
+  where: { rDate: { equals: new Date(`${scheduleOptions.year}-${zeroPadder.pad(scheduleOptions.month)}-${scheduleOptions.date}T00:00:00.000Z`) } }
+})
 
 interface SutTypes {
   sut: PrismaScheduleRepository
@@ -28,28 +63,11 @@ describe('PrismaScheduleRepository', () => {
     const findReplacementSpy = jest.spyOn(prisma.replacement, 'findMany')
     const findScheduleSpy = jest.spyOn(prisma.schedule, 'findFirst')
 
-    const scheduleOptions = {
-      weekDay: 6,
-      year: 2022,
-      month: 1,
-      date: 22
-    }
+    const scheduleOptions = makeFakeScheduleOptions()
     await sut.getPartial(scheduleOptions)
 
-    expect(findScheduleSpy).toHaveBeenCalledWith({
-      select: { duration: true, activationInterval: true, activationIntervalType: true }
-    })
-
-    expect(findIntervalTimeSpy).toHaveBeenCalledWith({
-      select: { week: true, timeFrom: true, timeTo: true },
-      orderBy: { timeFrom: 'asc' },
-      where: { week: { equals: scheduleOptions.weekDay } }
-    })
-
-    expect(findReplacementSpy).toHaveBeenCalledWith({
-      select: { rDate: true, rTimeFrom: true, rTimeTo: true },
-      orderBy: { rTimeFrom: 'asc' },
-      where: { rDate: { equals: new Date('2022-01-22T00:00:00.000Z') } }
-    })
+    expect(findScheduleSpy).toHaveBeenCalledWith(makeFindScheduleOptions())
+    expect(findIntervalTimeSpy).toHaveBeenCalledWith(makeFindTimeIntervalOptions(scheduleOptions))
+    expect(findReplacementSpy).toHaveBeenCalledWith(makeFindReplacementOptions(scheduleOptions))
   })
 })
