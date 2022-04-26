fa1ab814a925d267f487fe811d1acb4685ae3d3f
Wemerson Jesus <wejesuss@gmail.com>
2022-04-26 16:27:54 -0300

    test: ensure PrismaScheduleRepository calls find on appropriate models with correct values

---
+++ src/infra/repositories/prisma/prisma-schedule-repository/prisma-schedule-repository.spec.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaScheduleRepository } from './prisma-schedule-repository'
+
+interface SutTypes {
+  sut: PrismaScheduleRepository
+  prisma: PrismaClient
+}
+
+const prisma = new PrismaClient()
+const makeSut = (): SutTypes => {
+  const sut = new PrismaScheduleRepository(prisma)
+
+  return { sut, prisma }
+}
+
+describe('PrismaScheduleRepository', () => {
+  beforeEach(async () => {
+    await prisma.$connect()
+  })
+
+  afterAll(async () => {
+    await prisma.$disconnect()
+  })
+
+  test('Should call find on schedule, replacement and timeInterval with correct values', async () => {
+    const { sut, prisma } = makeSut()
+    const findIntervalTimeSpy = jest.spyOn(prisma.timeInterval, 'findMany')
+    const findReplacementSpy = jest.spyOn(prisma.replacement, 'findMany')
+    const findScheduleSpy = jest.spyOn(prisma.schedule, 'findFirst')
+
+    const scheduleOptions = {
+      weekDay: 6,
+      year: 2022,
+      month: 1,
+      date: 22
+    }
+    await sut.getPartial(scheduleOptions)
+
+    expect(findScheduleSpy).toHaveBeenCalledWith({
+      select: { duration: true, activationInterval: true, activationIntervalType: true }
+    })
+
+    expect(findIntervalTimeSpy).toHaveBeenCalledWith({
+      select: { week: true, timeFrom: true, timeTo: true },
+      orderBy: { timeFrom: 'asc' },
+      where: { week: { equals: scheduleOptions.weekDay } }
+    })
+
+    expect(findReplacementSpy).toHaveBeenCalledWith({
+      select: { rDate: true, rTimeFrom: true, rTimeTo: true },
+      orderBy: { rTimeFrom: 'asc' },
+      where: { rDate: { equals: new Date('2022-01-22T00:00:00.000Z') } }
+    })
+  })
+})
