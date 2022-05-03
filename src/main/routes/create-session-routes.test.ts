34d0f3a929a7ff785cacf28b23dba6599850b9f4
Wemerson Jesus <wejesuss@gmail.com>
2022-05-02 21:46:48 -0300

    test: add create-session route using express route adapter

--- src/main/routes/create-session-routes.test.ts
+++ src/main/routes/create-session-routes.test.ts
@@ -1,10 +1,70 @@
+import db from '../config/db'
 import app from '../config/app'
 import supertest from 'supertest'
+import { zeroPadder } from '../../shared/zero-pad/zero-pad'
 
 describe('CreateSession Route', () => {
-  test('Should return ok', async () => {
+  beforeAll(async () => {
+    await db.$connect()
+    await db.session.deleteMany()
+  })
+
+  afterAll(async () => {
+    await db.$disconnect()
+  })
+
+  test('Should return create session on success', async () => {
+    const today = new Date()
+    today.setDate(today.getDate() + 5)
+    const dTomorrow = today
+    const year = zeroPadder.pad(dTomorrow.getFullYear())
+    const month = zeroPadder.pad(dTomorrow.getMonth() + 1)
+    const date = zeroPadder.pad(dTomorrow.getDate())
+
     await supertest(app)
       .post('/api/create-session')
-      .expect(200, { ok: 'ok' })
+      .send({
+        name: 'Joe',
+        email: 'joeeleven@gmail.com',
+        phone: '11977889900',
+        cpf: '38054581777',
+        description: 'Some good description',
+        session_date: `${year}/${month}/${date}`,
+        session_time: '09:00'
+      }).expect((res) => {
+        const {
+          id,
+          name,
+          email,
+          cpf,
+          phone,
+          description,
+          duration,
+          s_date: sDate,
+          time_from: timeFrom,
+          time_to: timeTo,
+          price,
+          paid,
+          image_path: imagePath,
+          created_at: createdAt,
+          updated_at: updatedAt
+        } = res.body
+
+        expect(id).toBeTruthy()
+        expect(name).toBe('Joe')
+        expect(email).toBe('joeeleven@gmail.com')
+        expect(cpf).toBe('38054581777')
+        expect(phone).toBe('11977889900')
+        expect(description).toBe('Some good description')
+        expect(duration).toBe(60)
+        expect(sDate).toBe('2022/05/07')
+        expect(timeFrom).toBe('09:00:00')
+        expect(timeTo).toBe('10:00:00')
+        expect(price).toBe(10000)
+        expect(paid).toBe(false)
+        expect(imagePath).toBeFalsy()
+        expect(createdAt).toBeTruthy()
+        expect(updatedAt).toBeTruthy()
+      })
   })
 })
