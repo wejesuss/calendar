eb707a76fdeea5f2f928e792d1ed1b85854f4b3d
Wemerson Jesus <wejesuss@gmail.com>
2022-05-11 16:49:06 -0300

    feat: serve static files on public folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,17 +1,11 @@
-import { Express, Router } from 'express'
+import express from 'express'
 import fb from 'fast-glob'
 
-export default (app: Express): void => {
-  const apiRouter = Router()
-  const pagesRouter = Router()
+export default (app: express.Express): void => {
+  const apiRouter = express.Router()
   app.use('/api', apiRouter)
-  app.use('/', pagesRouter)
 
   fb.sync('**/src/main/routes/api/**routes.ts').forEach(async (file) => {
     (await import(`../../../${file}`)).default(apiRouter)
-  })
-
-  fb.sync('**/src/main/routes/pages/**routes.ts').forEach(async (file) => {
-    (await import(`../../../${file}`)).default(pagesRouter)
   })
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { bodyParser } from '../middlewares/body-parser'
 export { cors } from '../middlewares/cors'
 export { contentType } from '../middlewares/content-type'
+export { staticPublic } from '../middlewares/static-public-content'
