c15337281ed1e070d0f3b3261d3b9be992558a1d
Wemerson Jesus <wejesuss@gmail.com>
2022-05-11 16:01:26 -0300

    feat: ensure create session route returns create session page

--- src/main/routes/pages/create-session-routes.ts
+++ src/main/routes/pages/create-session-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import path from 'path'
 
 export default (router: Router): void => {
   router.get('/create-session', async (req, res) => {
-    return res.send({ ok: 'ok' })
+    const filePath = path.resolve(__dirname, '../../../../', 'public', 'create-session', 'index.html')
+
+    res.type('.html').sendFile(filePath)
   })
 }
