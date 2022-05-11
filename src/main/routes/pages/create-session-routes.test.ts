103bafa2b4792a539d291753ae37fc0583b128d9
Wemerson Jesus <wejesuss@gmail.com>
2022-05-11 16:01:48 -0300

    test: ensure create session route returns create session page

--- src/main/routes/pages/create-session-routes.test.ts
+++ src/main/routes/pages/create-session-routes.test.ts
@@ -1,10 +1,18 @@
 import app from '../../config/app'
 import supertest from 'supertest'
+import path from 'path'
+import { readFile } from 'fs/promises'
 
 describe('CreateSession Page Route', () => {
   test('Should return create session page on success', async () => {
+    const filePath = path.resolve(__dirname, '../../../../', 'public', 'create-session', 'index.html')
+    const fileContent = await readFile(filePath, { encoding: 'utf-8' })
+
     await supertest(app)
       .get('/create-session')
-      .expect(200)
+      .expect((res) => {
+        expect(res.statusCode).toBe(200)
+        expect(res.text).toBe(fileContent)
+      })
   })
 })
