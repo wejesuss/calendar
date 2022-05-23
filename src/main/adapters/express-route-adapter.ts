c0f988c4f3fc7d86879a5445658ee2282779b0b3
Wemerson Jesus <wejesuss@gmail.com>
2022-05-23 15:41:43 -0300

    feat: ensure express route adapter returns error name and message

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,6 +9,15 @@ export const adaptRoute = (controller: Controller) => {
 
     const httpResponse = await controller.handle(httpRequest)
 
+    if (httpResponse.statusCode >= 400) {
+      const error = {
+        name: httpResponse.body.name,
+        message: httpResponse.body.message
+      }
+
+      return res.status(httpResponse.statusCode).json(error)
+    }
+
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
