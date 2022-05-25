80f5b976fd83a3fa12bde74eaedd2b4c090ac952
Wemerson Jesus <wejesuss@gmail.com>
2022-05-25 18:55:55 -0300

    chore: add heroku postgres database configuration

--- src/main/config/db.ts
+++ src/main/config/db.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+if (process.env.NODE_ENV === 'production') {
+  process.env.DATABASE_URL = process.env.HEROKU_POSTGRESQL_PUCE_URL
+}
+
 export default new PrismaClient()
