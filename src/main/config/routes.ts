22c9d877ded4f1355c0c695230eccfeeeb96139d
Wemerson Jesus <wejesuss@gmail.com>
2022-05-25 19:33:43 -0300

    fix: set correct scripts and path resolution for development and production environment

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import fb from 'fast-glob'
+import path from 'path'
 
 export default (app: express.Express): void => {
   const apiRouter = express.Router()
   app.use('/api', apiRouter)
 
-  fb.sync('**/src/main/routes/api/**routes.ts').forEach(async (file) => {
-    (await import(`../../../${file}`)).default(apiRouter)
+  const cwd = path.resolve(__dirname, '../../')
+
+  fb.sync('main/routes/api/**routes.[jt]s', { cwd }).forEach(async (file) => {
+    (await import(`../../${file}`)).default(apiRouter)
   })
 }
