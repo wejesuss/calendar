d67798b2a7929e15f4d93e79563de1313c4abbd0
Wemerson Jesus <wejesuss@gmail.com>
2022-05-25 19:46:08 -0300

    chore: get port from process env or fallback to 5000

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,5 @@ import db from './config/db'
 db.$connect().then(async () => {
   const app = (await import('./config/app')).default
 
-  app.listen(5000, () => console.log('app listening on port 5000'))
+  app.listen(process.env.PORT || 5000, () => console.log('app listening on port 5000'))
 }).catch(console.error)
